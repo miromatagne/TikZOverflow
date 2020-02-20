1f40d135bf0ae95372b4686ac7e9a0c93bab1332
edgar-badalyan <edgar.badalyan1999@gmail.com>
2020-02-20 15:19:38 +0100

    LoginScreen layout

--- src/Model/FileHandler.java
+++ src/Model/FileHandler.java
@@ -1,6 +1,6 @@
 package Model;
 
-//import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 
--- src/View/LoginScreen.java
+++ src/View/LoginScreen.java
@@ -1,9 +1,11 @@
 package View;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,8 +19,17 @@ public class LoginScreen extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("TikZOverflow");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
         primaryStage.show();
+    }
+
+    public void login(ActionEvent actionEvent) {
+
+    }
+
+    public void newAccount(MouseEvent mouseEvent) {
+        System.out.println("test");
     }
 }
