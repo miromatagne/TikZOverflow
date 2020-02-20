2f889524e0fa4404ea64cdfcb5527e0b9615050c
rafftod <ramaluto@gmail.com>
2020-02-20 21:45:55 +0100

    Change cursor when mouse moves over TCU and remove unused imports

--- src/View/AccountCreation.java
+++ src/View/AccountCreation.java
@@ -1,15 +1,11 @@
 package View;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -32,23 +28,27 @@ public class AccountCreation extends Application {
         stage.setTitle("Account Creation");
         stage.setScene(new Scene(root));
         Text tcu = (Text) root.lookup("#termsAndConditionsText");
-        tcu.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                try {
-                    Parent tcuRoot = FXMLLoader.load(getClass().getResource("termsAndConditions.fxml"));
-                    stage.setScene(new Scene(tcuRoot));
-                    File f = new File("src/View/tcu.txt");
-                    BufferedReader br = new BufferedReader(new FileReader(f));
-                    String tmp, text = "";
-                    while((tmp = br.readLine()) != null) {
-                        text+=tmp+'\n';
-                    }
-                    Text tcuFullText = (Text) tcuRoot.lookup("#tcuFullText");
-                    tcuFullText.setText(text);
-                } catch (Exception e) {
-                    e.printStackTrace();
+        tcu.setOnMouseClicked(mouseEvent ->  {
+            try {
+                Parent tcuRoot = FXMLLoader.load(getClass().getResource("termsAndConditions.fxml"));
+                stage.setScene(new Scene(tcuRoot));
+                File f = new File("src/View/tcu.txt");
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                String tmp, text = "";
+                while((tmp = br.readLine()) != null) {
+                    text = text.concat(tmp+'\n');
                 }
+                Text tcuFullText = (Text) tcuRoot.lookup("#tcuFullText");
+                tcuFullText.setText(text);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        tcu.setOnMouseMoved(mouseEvent -> {
+            try {
+                tcu.setCursor(Cursor.HAND);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
         stage.show();
