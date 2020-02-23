4d7e4bcc8ea43697b6e6fd6843047ca51f09c124
alexandre <alexandremissenard@gmail.com>
2020-02-23 10:32:17 +0100

    Ajout d'un return bouton a l'ecran de modification. Ajout de commentaire permettant de comprendre le role des objets crees. Lors de l'interraction avec le bouton validate lecture des différents TextField afin de recupere les information qui y sont contenues.

--- src/Controller/Modification_Controller.java
+++ src/Controller/Modification_Controller.java
@@ -4,10 +4,19 @@ import View.ScreenHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
+/*Controller for the modification screen that contains the method used by the button or textfield :
+    - Validate button : if clicked we read all the TextField and use the save object in Model in order to modified
+      the username.txt.
+    - Return button : if clicked the user return to the main screen and we notify him if he want to save the new data
+      before quitting.
+ */
 public class Modification_Controller implements Initializable
 {
 
@@ -23,9 +32,23 @@ public class Modification_Controller implements Initializable
     }
 
     @FXML
-    public void validate_button_action(javafx.event.ActionEvent actionEvent)
+    public void validate_button_action(ActionEvent actionEvent)
     {
-        System.out.println("Save ok !");
+        TextField usernameField =  (TextField)ScreenHandler.get_current_scene().lookup("#usernameField") ;
+        TextField firstNameField = (TextField)ScreenHandler.get_current_scene().lookup("#firstNameField") ;
+        TextField lastNameField = (TextField)ScreenHandler.get_current_scene().lookup("#lastNameField") ;
+        TextField emailField = (TextField)ScreenHandler.get_current_scene().lookup("#emailField") ;
+        TextField passwordField = (TextField)ScreenHandler.get_current_scene().lookup("#passwordField") ;
+        TextField passwordField1 = (TextField)ScreenHandler.get_current_scene().lookup("#passwordField1") ;
+
+        System.out.println("Validate button clicked");
+
+        /*Work in progress need the object save from Model in order to continue */
     }
 
+    @FXML
+    public void return_button_action(ActionEvent actionEvent)
+    {
+        ScreenHandler.change_scene(0);
+    }
 }
--- src/View/ScreenHandler.java
+++ src/View/ScreenHandler.java
@@ -9,6 +9,15 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 
+/*Class used to contain all the FXML in an ArrayList in order to switch between screen using a static method.
+    -void main() for the application that launch the start method
+    -void start(Stage stage) will initialize all the object from the class and fill the ArrayList of FXML view
+    -void change_scene(int) static method that takes an int which will be the id of the new screen loaded. Static because
+     it will be called by other objects such as button controller
+    -Pane get_current_scene() return the actual scene which is a Pane object
+    -int get_current_id() return the current id of the actual loaded view
+ */
+
 public class ScreenHandler extends Application {
     //Attribut
     static ArrayList<Pane> all_scene = new ArrayList<Pane>() ;
@@ -18,16 +27,19 @@ public class ScreenHandler extends Application {
     //Methode
     public static void change_scene(int id_scene)
     {
-        if(id_scene != id_current){root.getChildren().remove(id_current);}
+        if(id_scene != id_current){root.getChildren().remove(get_current_scene());}
         id_current = id_scene ;
         root.getChildren().add(all_scene.get(id_scene));
+        System.out.println(id_current);
     }
 
-    public static Pane get_scene(int id_scene){return all_scene.get(id_scene);}
+    public static Pane get_current_scene(){return all_scene.get(id_current);}
+    public static int get_id_current(){return id_current;}
+
 
     public static void main(String[] args) {
         try{ launch(args);}
-        catch(Exception expc){System.out.println("Nop");}
+        catch(Exception expc){System.out.println("Error in launching the start() method");}
     }
 
     @Override
