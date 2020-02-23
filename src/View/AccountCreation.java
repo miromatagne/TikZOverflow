9755593e40d5aedd3b3b8a85a6236edbeacff822
alosfeld <armand.losfeld@ulb.ac.be>
2020-02-23 16:51:59 +0100

    Ajout d'un bouton pour le retour sur la page de création lorqu'on est sur la page Terms And Conditions.

--- src/View/AccountCreation.java
+++ src/View/AccountCreation.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -25,13 +26,19 @@ public class AccountCreation extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("accountCreation.fxml"));
+        Parent tcuRoot = FXMLLoader.load(getClass().getResource("termsAndConditions.fxml"));
+        Scene scene_root = new Scene(root);
+        Scene scene_tcuRoot = new Scene(tcuRoot);
+
         stage.setTitle("Account Creation");
-        stage.setScene(new Scene(root));
+        stage.setScene(scene_root);
+
         Text tcu = (Text) root.lookup("#termsAndConditionsText");
         tcu.setOnMouseClicked(mouseEvent ->  {
             try {
-                Parent tcuRoot = FXMLLoader.load(getClass().getResource("termsAndConditions.fxml"));
-                stage.setScene(new Scene(tcuRoot));
+                stage.setScene(scene_tcuRoot);
+                stage.setTitle("Terms and conditions");
+
                 File f = new File("src/View/tcu.txt");
                 BufferedReader br = new BufferedReader(new FileReader(f));
                 String tmp, text = "";
@@ -47,6 +54,15 @@ public class AccountCreation extends Application {
         tcu.setOnMouseMoved(mouseEvent -> {
             try {
                 tcu.setCursor(Cursor.HAND);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        Button quitTcu = (Button) tcuRoot.lookup("#quitTcuButton");
+        quitTcu.setOnMouseClicked(mouseEvent -> {
+            try {
+                stage.setScene(scene_root);
+                stage.setTitle("Account creation");
             } catch (Exception e) {
                 e.printStackTrace();
             }
