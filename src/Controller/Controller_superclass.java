fe3195df1e13cebbc5ce7566e4bdb3e748104020
alexandre <alexandremissenard@gmail.com>
2020-02-27 13:49:23 +0100

    Merge branch 'H04-Code_Interface' of C:\IntelliJ_projet\groupe08 with conflicts.

---
+++ src/Controller/Controller_superclass.java
@@ -0,0 +1,10 @@
+package Controller;
+
+/*
+    This is a SuperClass for all the controller, it possess an abstract method update which will be called by
+    the screen Handler whenever a scene (fxml) associated with this controller needs to be update/reload.
+ */
+
+abstract public class Controller_superclass {
+    abstract public void update();
+}
