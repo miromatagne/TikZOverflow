70cc783fe94f2a1cc6e810de8e47c7c1f186d745
minh28 <minhx299@gmail.com>
2020-03-14 18:35:17 +0100

    classes pour les noeuds

--- src/Model/Shapes/Circle.java
+++ src/Model/Shapes/Circle.java
@@ -2,10 +2,11 @@ package Model.Shapes;
 
 public class Circle extends Node {
     private int radius;
-    public Circle(xCenter,yCenter){
+    public Circle(int xCenter,int yCenter){
         super(xCenter,yCenter);
     }
     public void setRadius(int radius) {
         this.radius = radius;
     }
+    public int getRadius(){return radius;}
 }
--- src/Model/Shapes/Rectangle.java
+++ src/Model/Shapes/Rectangle.java
@@ -1,14 +1,16 @@
 package Model.Shapes;
 
 public class Rectangle extends Node {
-    private int rectangleLength,rectangleWidth;
+    private int height,width;
     public Rectangle(int xCenter, int yCenter){
         super(xCenter,yCenter);
     }
-    public void setRectangleLength(int rectangleLength) {
-        this.rectangleLength = rectangleLength;
+    public void setHeight(int height) {
+        this.height = height;
     }
-    public void setRectangleWidth(int rectangleWidth) {
-        this.rectangleWidth = rectangleWidth;
+    public void setWidth(int width) {
+        this.width = width;
     }
+    public int getHeight(){return height;}
+    public int getWidth(){return width;}
 }
