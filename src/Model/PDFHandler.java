91c8e9442a8de554622aa5edf068108a7efcb15f
rafftod <ramaluto@gmail.com>
2020-03-18 12:25:29 +0100

    Add comments

--- src/Model/PDFHandler.java
+++ src/Model/PDFHandler.java
@@ -8,13 +8,24 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Class used to handle PDF files.
+ */
 public class PDFHandler {
     private String pdfPath;
 
+    /**
+     * Constructor
+     * @param pdfPath   path to the pdf file to be treated
+     */
     public PDFHandler(String pdfPath){
         this.pdfPath = pdfPath;
     }
 
+    /**
+     * Converts pdf file to PNG image file in the same directory
+     * @throws Exception    if pdf file doesn't exist
+     */
     public void convertPdfToImage() throws Exception {
         File file = new File(pdfPath);
         PDDocument document = PDDocument.load(file);
