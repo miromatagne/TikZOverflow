40945f8f4bd238790c5273f0b2857d9aae51727e
alexandre <alexandremissenard@gmail.com>
2020-03-29 12:02:51 +0200

    Add a test for the LatexController

---
+++ test/Controller/TestLatexController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import Model.User;
+import View.ControllerSuperclass;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestLatexController {
+
+    @Test
+    void getTextInFile() {
+        LatexController latexControl = new LatexController(null);
+        User user = new User();
+        user.setUsername("test");
+        Session.getInstance().setUser(user);
+        String stringTest = latexControl.getTextInFile();
+
+        assertEquals("\\documentclass{standalone}\n" +
+                "\n" +
+                "\\usepackage{tikz}\n" +
+                "\n" +
+                "\\begin{document}\n" +
+                "    \\begin{tikzpicture}\n" +
+                "        \\node (h) at (0,0) {Hello};\n" +
+                "        \\node (w) at (2,3) {World};\n" +
+                "        \\draw (h) edge (w);\n" +
+                "    \\end{tikzpicture}\n" +
+                "\\end{document}\n",
+                stringTest);
+    }
+
+}
