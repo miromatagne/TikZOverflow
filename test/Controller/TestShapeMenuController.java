21b1a6c9b3401949eeff0a978c1c015adee5b156
ftrouill <ftrouill@ulb.ac.be>
2020-04-27 16:34:46 +0200

    adding comments to TestShapeMenuController

--- test/Controller/TestShapeMenuController.java
+++ test/Controller/TestShapeMenuController.java
@@ -1,9 +1,10 @@
 package Controller;
 
-
+/**
+ * This class is not testable because we need a stage when we create it, so, it is out of the model and view only and
+ * it can not be considered as a unit test
+ */
 class TestShapeMenuController {
-
-
 
 
 }
