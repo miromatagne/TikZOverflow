27e68899ec188dbe1d1d71682662213ff41c74e7
ftrouill <ftrouill@ulb.ac.be>
2020-04-27 17:19:54 +0200

    adding comments to Tests

--- test/Controller/TestSession.java
+++ test/Controller/TestSession.java
@@ -1,20 +1,18 @@
 package Controller;
 
 import Controller.Exceptions.SessionOpeningException;
-import Model.Exceptions.FileHandlerConstructorException;
-import Model.FileHandler;
 import Model.User;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestSession {
     User testUser;
 
 
     @Test
-    public void testSetup() throws FileHandlerConstructorException {
-        FileHandler fh = new FileHandler();
+    public void testSetup() {
+
         testUser = new User();
         testUser.setUsername("ftrouill");
         testUser.setLastName("Trouillez");
@@ -23,21 +21,21 @@ class TestSession {
         testUser.setPassword("123456789");
     }
     @Test
-    public void userNotRegisteredTest() throws SessionOpeningException, FileHandlerConstructorException {
+    public void userNotRegisteredTest() throws SessionOpeningException {
         testSetup();
         int res1 = Session.getInstance().openSession("ftrouuuuil", "123456789");
         assertEquals(res1, Session.USER_NOT_REGISTERED);
     }
 
     @Test
-    public void wrongPasswordTest() throws SessionOpeningException, FileHandlerConstructorException {
+    public void wrongPasswordTest() throws SessionOpeningException {
         testSetup();
         int res2 = Session.getInstance().openSession("ftrouill", "oups");
         assertEquals(res2, Session.INVALID_PASSWORD);
     }
 
     @Test
-    public void successfulConnectionTest() throws SessionOpeningException, FileHandlerConstructorException {
+    public void successfulConnectionTest() throws SessionOpeningException {
         testSetup();
         int res3 = Session.getInstance().openSession("ftrouill", "123456789");
         assertEquals(res3, Session.CONNECTION_ESTABLISHED);
--- test/Controller/TestShapeMenuController.java
+++ test/Controller/TestShapeMenuController.java
@@ -1,7 +1,7 @@
 package Controller;
 
 /**
- * This class is not testable because we need a stage when we create it, so, it is out of the model and view only and
+ * This class is not testable because we need a stage when we create it, so, it is out of the model and controller only and
  * it can not be considered as a unit test
  */
 class TestShapeMenuController {
